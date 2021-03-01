68d91c5419377490b9fa0acaf2967a3fc7832ef0
Astha <16asthanema@gmail.com>
2021-03-01 22:22:19 +0530

    initial commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  newPost='No Content';
+  enteredPost='';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddPost(){
+    this.newPost=this.enteredPost;
+  }
+}
